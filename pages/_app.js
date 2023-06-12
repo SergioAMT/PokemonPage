7c8365bb1290f17c7f4b7142088b222b511de140
Sergio <alejandrosamt@gmail.com>
2023-06-11 20:44:32 -0400

    insertando estilos individuales

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { GlobalProvider } from '../content/global';
-import '../src/app/globals.css'
+import '../src/styles/globals.css'
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
